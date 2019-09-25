6cab996ab869b24adab894ed1b28e27d9f8ccfcc
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-25 02:41:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,11 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+movies_was_made_in_2018_2019 =	{
+	  :ALLEYESONME 
+	  :marvalEndGame
+	  :Hobbandshaw
+	}
 end
 
 def monopoly_with_second_tier
